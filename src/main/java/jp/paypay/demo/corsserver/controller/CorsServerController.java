02e0ae3c6d90c9b9e2d4d283c97589761390e8c6
Nikki-Kh <red-dragoon@inbox.ru>
2021-02-16 11:51:45 +0300

    init

---
+++ src/main/java/jp/paypay/demo/corsserver/controller/CorsServerController.java
@@ -0,0 +1,29 @@
+package jp.paypay.demo.corsserver.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController("/")
+public class CorsServerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CorsServerController.class);
+
+    @GetMapping("/cors-enabled")
+    @CrossOrigin
+    public String returnOkStringFromCorsRequest() {
+        return "ok";
+    }
+
+    @PostMapping("cors-disabled")
+    public String getErrorFromCorsRequest() {
+        return "ok";
+    }
+}
---
+++ src/test/java/jp/paypay/demo/corsserver/CorsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.paypay.demo.corsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
